957afdc03f8b05959f487a055185efdd5b477715
marvin88-dev <61692526+marvin88-dev@users.noreply.github.com>
2022-03-14 10:53:44 +0100

    update

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Setup End Date for Countdown (getTime == Time in Milleseconds)
-let launchDate = new Date("mar 15, 2022 20:00:00").getTime();
+let launchDate = new Date("mar 14, 2022 12:00:00").getTime();
 
 // Setup Timer to tick every 1 second
 let timer = setInterval(tick, 1000);
@@ -12,11 +12,6 @@ function tick () {
 
   // Check if time is above 0
   if (t > 0) {
-    // Setup Days, hours, seconds and minutes
-    // Algorithm to calculate days...
-    let days = Math.floor(t / (1000 * 60 * 60 * 24));
-    // prefix any number below 10 with a "0" E.g. 1 = 01
-    if (days < 10) { days = "0" + days; }
     
     // Algorithm to calculate hours
     let hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
@@ -31,7 +26,7 @@ function tick () {
     if (secs < 10) { secs = "0" + secs; }
 
     // Create Time String
-    let time = `${days} : ${hours} : ${mins} : ${secs}`;
+    let time = `${hours} : ${mins} : ${secs}`;
 
     // Set time on document
     document.querySelector('.countdown').innerText = time;
